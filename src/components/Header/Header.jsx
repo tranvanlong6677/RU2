d55eddf3695411e73a01bf341c01ea4119344c7b
“Tran <tranvanlong6677@gmail.com>
2023-04-12 23:16:13 +0700

    active link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {Link} from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link,NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <div>
@@ -13,22 +13,22 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" to="/users">Users</Link>
-            <Link className="nav-link" to="/admin">Admin</Link>
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+              <NavLink className="nav-link" to="/users">
+                Users
+              </NavLink>
+              <NavLink className="nav-link" to="/admin">
+                Admin
+              </NavLink>
             </Nav>
             <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.2">
-                  Login
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Logout
-                </NavDropdown.Item>
+              <NavDropdown title="Settings" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.2">Login</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Logout</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                 Profile
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Profile</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
