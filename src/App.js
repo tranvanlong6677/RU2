08a5efbe68a2e65ff2931ad226d5514494e2ebfc
“Tran <tranvanlong6677@gmail.com>
2023-04-13 12:24:25 +0700

    video homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header.jsx";
 import { Outlet } from "react-router-dom";
 const App = () => {
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import videoHomePage from '../../assets/video/homePage.webm';
 
 const HomePage = () => {
-    return (
-        <div className="homepage-container">
-            HomePage
-        </div>
-    );
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
